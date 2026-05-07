492ff21d6fe9784d39302689ed05686adf119fff
YixingTRD2943 <2943670672@qq.com>
2026-05-08 01:19:16 +0800

    修复 index.js 路径问题和 TrackPlayer service 注册

--- index.js
+++ index.js
@@ -4,8 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import TrackPlayer from 'react-native-track-player';
-import Pages from '@/entry';
+import Pages from './src/entry/index';
 
 AppRegistry.registerComponent(appName, () => Pages);
-TrackPlayer.registerPlaybackService(() => require('./src/service/index'));
